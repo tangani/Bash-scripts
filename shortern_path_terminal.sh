7f0dc7b9744318655c9af4926c03482f872dfc5d
tangani <tanganimoyo@gmail.com>
2020-07-11 11:33:06 +0200

    added more details describing usage

--- shortern_path_terminal.sh
+++ shortern_path_terminal.sh
@@ -1,7 +1,40 @@
 #!/usr/bin/bash
 
+function usage() {
+	
+	echo "ensure the shebang corresponds with your shell enviroment"
+	echo "run the script with a . to execute in the same shell"
+	echo -e "\e[32m			. shorten_shell_path.sh "
+}
+
+function colors() {
+	echo -e "Default \e[30mBlack"				
+	echo -e "Default \e[31mRed"					
+	echo -e "Default \e[32mGreen"
+	echo -e "Default \e[33mYellow"
+	echo -e "Default \e[34mBlue"
+	echo -e "Default \e[35mMagenta"
+	echo -e "Default \e[36mCyan"
+	echo -e "Default \e[37mLight gray"
+	echo -e "Default \e[90mDark gray"
+	echo -e "Default \e[91mLight red"
+	echo -e "Default \e[92mLight green"
+	echo -e "Default \e[93mLight yellow"
+	echo -e "Default \e[94mLight blue"
+	echo -e "Default \e[95mLight magenta"
+	echo -e "Default \e[96mLight cyan"
+	echo -e "Default \e[97mWhite"
+}
+
+
+usage
+
 PS1='\e[1;34m\u@\H:\e[0m\e[1;33m\W\e[0m\e[1;35m$\e[0m ' # This will give: user@pc_name:cwd
-echo "Changed"
+echo -e "\e[97mChanged"
+
+
+
+
 
 : ' 
 
